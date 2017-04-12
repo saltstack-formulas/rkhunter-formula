1aec833ff3cb1372fc36d63ff121b0496c07e866
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2017-04-13 01:57:59 +0200

    Add tests with Kitchen Test

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+ruby '2.3.4'
+
+gem 'test-kitchen'
+gem 'kitchen-docker'
+gem 'kitchen-salt', git: 'https://github.com/simonmcc/kitchen-salt.git'
+gem 'kitchen-inspec'
+gem 'rake'
---
+++ test/integration/base/controls/rkhunter.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+title 'Test Rkhunter installation'
+
+describe package('rkhunter') do
+  it { should be_installed }
+end
+
+describe file('/etc/rkhunter.conf') do
+  # Default config
+  its('content') { should include 'AUTO_X_DETECT=1' }
+  its('content') { should include 'DISABLE_UNHIDE=1' }
+  its('content') { should include 'DBDIR=/var/lib/rkhunter/db' }
+  its('content') { should include 'SCRIPTWHITELIST=/bin/egrep' }
+  its('content') { should include 'SCRIPTWHITELIST=/bin/fgrep' }
+  its('content') { should include 'SCRIPTWHITELIST=/bin/which' }
+  its('content') { should include 'SCRIPTWHITELIST=/usr/bin/groups' }
+  its('content') { should include 'SCRIPTWHITELIST=/usr/bin/ldd' }
+  its('content') { should include 'SCRIPTWHITELIST=/usr/sbin/adduser' }
+  its('content') { should include 'LOGFILE=/var/log/rkhunter.log' }
+  its('content') { should include 'INSTALLDIR=/usr' }
+  its('content') { should include 'ENABLE_TESTS=all' }
+  its('content') { should include 'TMPDIR=/var/lib/rkhunter/tmp' }
+  its('content') { should include 'SCRIPTDIR=/usr/share/rkhunter/scripts' }
+  its('content') { should include "DISABLE_TESTS='suspscan hidden_procs deleted_files packet_cap_apps apps'" }
+
+  # Custom config from pillar
+  its('content') { should include 'ALLOW_SSH_ROOT_USER=yes' }
+end
